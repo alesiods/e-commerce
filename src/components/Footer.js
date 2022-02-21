bba1f98e57fa41b382ef43164d9b6c315c3712ac
alesiods <alesioadrover@gmail.com>
2022-02-21 17:33:46 -0300

    Desafio clase 3

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+        <p>&copy; Copyright 2022 - Alesio Adrover</p>
+        <p>Todos los derechos reservados</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+        <h1>Tienda Online</h1>
+        <nav>
+          <ul> 
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Contacto</a></li>
+          </ul>        
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
