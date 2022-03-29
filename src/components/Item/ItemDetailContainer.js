f80d7fedf3a3eaaafbc80bb0bd6650f9a15c9939
alesiods <alesioadrover@gmail.com>
2022-03-29 17:21:45 -0300

    desaffio 12

--- src/components/Item/ItemDetailContainer.js
+++ src/components/Item/ItemDetailContainer.js
@@ -15,9 +15,7 @@ const ItemDetailContainer = () => {
   const {idProducto} = useParams()
 
     useEffect (() => {
-
-    
-
+      
       const filtroProductos = query(coleccionProductos,where("id","==",Number(idProducto)))
       const pedido = getDocs(filtroProductos)
 
@@ -25,34 +23,7 @@ const ItemDetailContainer = () => {
           .then(resultado => setProductos(resultado.docs[0].data()))
           .catch(() => toast.error("Error al cargar la informacion"))
           .finally(() => setLoading(false))
-
-
-      /* toast.info("Trayendo la informacion...",{theme: "dark"}
-    )
-      const pedido = new Promise ((res,rej)=>{
-        setTimeout(() => {
-          const detalle = productosIniciales.find(function(element){ 
-            return element.id == idProducto; 
-          });
-          return res(detalle)
-        }
-        , 500);
-      })
-
-      pedido
-      .then((resultado)=>{
-        toast.dismiss()
-        setProductos(resultado)
-        console.log ("Carga exitosa")
-      })
-
-      .catch((error)=>{
-        toast.error ("Error al cargar la informacion")
-      })
-      .finally (()=>{
-        setLoading(false)
-      })
- */
+     
     }, [idProducto])
 
     return (
@@ -61,15 +32,6 @@ const ItemDetailContainer = () => {
         <ItemDetail prod={productos}/>   
         </section>)
     )
-
-    /* if(loading){
-      return <h4>Cargando Detalle</h4>
-    } else{
-  return (
-    <section> 
-    <ItemDetail prod={productos}/>   
-    </section>
-  )
-} */}
+}
 
 export default ItemDetailContainer
